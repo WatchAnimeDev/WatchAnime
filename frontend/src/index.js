00cab38811b206d3d1dc1d6c58d9b98c38a8b350
Shounak Ghosh <sghosh1810@gmail.com>
2022-09-06 20:49:22 +0530

    Added sentry

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,18 @@ import reportWebVitals from "./reportWebVitals";
 import { MantineProvider } from "@mantine/core";
 import { NotificationsProvider } from "@mantine/notifications";
 import { ModalsProvider } from "@mantine/modals";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+    dsn: "https://a84854924fe143aea950783419453fc9@o412163.ingest.sentry.io/6723311",
+    integrations: [new BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 

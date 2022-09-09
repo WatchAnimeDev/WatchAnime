63c3cbd5d7df8506fcbde30b7da3c84bde0a03f1
Shounak Ghosh <sghosh1810@gmail.com>
2022-09-09 22:56:37 +0530

    Misc updates

--- frontend/public/serviceWorker.js
+++ frontend/public/serviceWorker.js
@@ -1,14 +1,5 @@
 let CACHE_NAME = "watchanime-cache-v1";
 const urlsToCache = ["/", "/index.html"];
-self.addEventListener("install", function (event) {
-    // Perform install steps
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
-            console.log("Opened cache");
-            return cache.addAll(urlsToCache);
-        })
-    );
-});
 self.addEventListener("fetch", function (event) {
     event.respondWith(
         caches.match(event.request).then(function (response) {

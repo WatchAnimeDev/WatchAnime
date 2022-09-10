23a54eadb2c5248bea64933ad407d536e6a7382b
Shounak Ghosh <sghosh1810@gmail.com>
2022-09-10 21:52:07 +0530

    Update endpoint

--- frontend/src/constants/genricConstants.js
+++ frontend/src/constants/genricConstants.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = "https://watchanime-recode-main-api.waprimary.deployer.in";
+export const API_BASE_URL = "https://api.watchanime.dev";
 export const API_REDIRECT_HOST = "https://in.watchanime.dev/";
 export const GOGO_DOWNLOAD_LINK = "https://gogohd.net";

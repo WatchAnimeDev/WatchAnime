c88521186bd3fce51db56fe123d8838ecff64428
sghosh1810 <sghosh1810@gmail.com>
2022-10-25 10:26:52 +0530

    Added uid

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,7 @@ import DmcaScreen from "./screens/DmcaScreen";
 import PrivacyPolicyScreen from "./screens/PrivacyPolicyScreen";
 import BugReportLayout from "./layouts/BugReportLayout";
 import ContactScreen from "./screens/ContactScreen";
+import { getOrSetUid } from "./custom/User";
 
 function App() {
     const [sideBarState, setSideBarState] = useState(false);
@@ -46,10 +47,22 @@ function App() {
                         <Routes>
                             <Route
                                 path="/"
-                                element={<HomeScreen sideBarState={sideBarState} setSideBarState={setSideBarState} bugReportState={bugReportState} setBugReportState={setBugReportState} otherData={{ targetRefSchedule: targetRefSchedule }} />}
+                                element={
+                                    <HomeScreen
+                                        sideBarState={sideBarState}
+                                        setSideBarState={setSideBarState}
+                                        bugReportState={bugReportState}
+                                        setBugReportState={setBugReportState}
+                                        otherData={{ targetRefSchedule: targetRefSchedule }}
+                                    />
+                                }
                                 exact
                             ></Route>
-                            <Route path="/anime/:animeSlug" element={<AnimeDetailsScreen sideBarState={sideBarState} setSideBarState={setSideBarState} bugReportState={bugReportState} setBugReportState={setBugReportState} />} exact></Route>
+                            <Route
+                                path="/anime/:animeSlug"
+                                element={<AnimeDetailsScreen sideBarState={sideBarState} setSideBarState={setSideBarState} bugReportState={bugReportState} setBugReportState={setBugReportState} />}
+                                exact
+                            ></Route>
                             <Route
                                 path="/anime/:animeSlug/episode/:episodenumber"
                                 element={<VideoPlayerScreen sideBarState={sideBarState} setSideBarState={setSideBarState} bugReportState={bugReportState} setBugReportState={setBugReportState} />}
@@ -57,20 +70,48 @@ function App() {
                             ></Route>
                             <Route
                                 path="/recent/:pageNumber"
-                                element={<GenericScreen sideBarState={sideBarState} setSideBarState={setSideBarState} pageType={"recent"} hasPagination={true} bugReportState={bugReportState} setBugReportState={setBugReportState} />}
+                                element={
+                                    <GenericScreen
+                                        sideBarState={sideBarState}
+                                        setSideBarState={setSideBarState}
+                                        pageType={"recent"}
+                                        hasPagination={true}
+                                        bugReportState={bugReportState}
+                                        setBugReportState={setBugReportState}
+                                    />
+                                }
                             ></Route>
                             <Route
                                 path="/popular/:pageNumber"
-                                element={<GenericScreen sideBarState={sideBarState} setSideBarState={setSideBarState} pageType={"popular"} hasPagination={true} bugReportState={bugReportState} setBugReportState={setBugReportState} />}
+                                element={
+                                    <GenericScreen
+                                        sideBarState={sideBarState}
+                                        setSideBarState={setSideBarState}
+                                        pageType={"popular"}
+                                        hasPagination={true}
+                                        bugReportState={bugReportState}
+                                        setBugReportState={setBugReportState}
+                                    />
+                                }
                             ></Route>
-                            <Route path="/dmca" element={<DmcaScreen sideBarState={sideBarState} setSideBarState={setSideBarState} bugReportState={bugReportState} setBugReportState={setBugReportState} />}></Route>
-                            <Route path="/privacy" element={<PrivacyPolicyScreen sideBarState={sideBarState} setSideBarState={setSideBarState} bugReportState={bugReportState} setBugReportState={setBugReportState} />}></Route>
-                            <Route path="/contact" element={<ContactScreen sideBarState={sideBarState} setSideBarState={setSideBarState} bugReportState={bugReportState} setBugReportState={setBugReportState} />}></Route>
+                            <Route
+                                path="/dmca"
+                                element={<DmcaScreen sideBarState={sideBarState} setSideBarState={setSideBarState} bugReportState={bugReportState} setBugReportState={setBugReportState} />}
+                            ></Route>
+                            <Route
+                                path="/privacy"
+                                element={<PrivacyPolicyScreen sideBarState={sideBarState} setSideBarState={setSideBarState} bugReportState={bugReportState} setBugReportState={setBugReportState} />}
+                            ></Route>
+                            <Route
+                                path="/contact"
+                                element={<ContactScreen sideBarState={sideBarState} setSideBarState={setSideBarState} bugReportState={bugReportState} setBugReportState={setBugReportState} />}
+                            ></Route>
                         </Routes>
                     </Container>
                 </main>
                 <BugReportLayout bugReportState={bugReportState} setBugReportState={setBugReportState} />
                 <FooterComponent />
+                {getOrSetUid()}
             </Router>
         </SpotlightProvider>
     );
---
+++ frontend/src/custom/User.js
@@ -0,0 +1,18 @@
+function uuidv4() {
+    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) => (c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16));
+}
+function getUid() {
+    return localStorage.getItem("uid");
+}
+function setUid() {
+    localStorage.setItem("uid", uuidv4());
+}
+function getOrSetUid() {
+    if (getUid()) {
+        return getUid();
+    }
+    setUid();
+    return getUid();
+}
+
+export { getOrSetUid };

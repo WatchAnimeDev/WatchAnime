abf5d2e81a1af6c3b1cbffa1ca5819181ad90819
Shounak Ghosh <sghosh1810@gmail.com>
2024-02-18 02:19:22 +0530

    Support for remote cache

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 ```
 cd frontend
+npm run build
 tar -cvf ./deploy.tar --exclude='*.map' ./captain-definition ./build/*
 ```
--- frontend/src/custom/User.js
+++ frontend/src/custom/User.js
@@ -1,11 +1,21 @@
+import axios from "axios";
+import { API_BASE_URL } from "../constants/genricConstants";
+
 function uuidv4() {
     return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) => (c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16));
 }
 function getUid() {
     return localStorage.getItem("uid");
 }
-function setUid() {
-    localStorage.setItem("uid", uuidv4());
+async function setUid() {
+    let uid = null;
+    try {
+        let uidData = await axios.get(`${API_BASE_URL}/auth/uid/set`);
+        uid = uidData.data?.anonUid || uuidv4();
+    } catch (error) {
+        uid = uuidv4();
+    }
+    localStorage.setItem("uid", uid);
 }
 function getOrSetUid() {
     if (getUid()) {

1b1a74e025b0285c5896b07ce0e75d7a0440a89f
Shounak Ghosh <sghosh1810@gmail.com>
2024-03-24 18:35:22 +0530

    move to v1 of api for v2 release in future

--- frontend/src/constants/genricConstants.js
+++ frontend/src/constants/genricConstants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "https://api.watchanime.dev";
+export const API_BASE_URL = "https://api.watchanime.dev/v1";
 export const API_REDIRECT_HOST = "https://in.watchanime.dev/";
 export const NOTIFICATION_BASE_URL = "https://notifications.watchanime.dev";
 export const GOGO_DOWNLOAD_LINK = "https://goone.pro";

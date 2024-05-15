4f80e18bb6f52019d452aa6cf88de16ccd35d825
Shounak Ghosh <sghosh1810@gmail.com>
2024-05-15 20:06:45 +0530

    Update to schedule names

--- frontend/src/partials/ScheduleForSelectedDatePartial.js
+++ frontend/src/partials/ScheduleForSelectedDatePartial.js
@@ -2,7 +2,11 @@ import React from "react";
 import { Group, Text, UnstyledButton } from "@mantine/core";
 import { IconDeviceTv } from "@tabler/icons";
 import { Link } from "react-router-dom";
+import { getAnimeTitleByRelevance } from "../custom/AnimeData";
+import { useLanguageStore } from "../store/LanguageToggleStore";
+import { useShallow } from "zustand/react/shallow";
 function ScheduleForSelectedDatePartial({ scheduleData, selectedDate }) {
+    const { language } = useLanguageStore(useShallow((state) => ({ language: state.language })));
     return (
         <Group mt={30} sx={{ gap: 0 }}>
             {scheduleData
@@ -15,7 +19,7 @@ function ScheduleForSelectedDatePartial({ scheduleData, selectedDate }) {
                         <Group key={ind} sx={{ width: "100%", backgroundColor: `${ind % 2 === 0 ? "rgb(37, 38, 43)" : "#2A2B2C"}`, flexWrap: "nowrap" }} p="sm">
                             <Text sx={{ width: "18%" }}>{schedule.timeday}</Text>
                             <Text sx={{ width: "70%" }} lineClamp={1}>
-                                {schedule.name}
+                                {getAnimeTitleByRelevance(schedule.titles, false, language)}
                             </Text>
                             <UnstyledButton sx={{ padding: "10px", backgroundColor: "rgb(26, 27, 30)", borderRadius: "10px", display: "flex", alignItems: "center" }} component={Link} to={`/anime/${schedule.slug}`}>
                                 <IconDeviceTv size={16} stroke={1.5} />

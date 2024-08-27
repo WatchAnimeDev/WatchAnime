fb00cb5b862aac95c9a402fcf51a4457cb1496d4
Shounak Ghosh <sghosh1810@gmail.com>
2024-08-27 19:11:14 +0530

    New download domain

--- frontend/src/constants/genricConstants.js
+++ frontend/src/constants/genricConstants.js
@@ -6,7 +6,7 @@ export const API_REDIRECT_HOST = "https://in.watchanime.dev/";
 export const NOTIFICATION_BASE_URL = "https://notifications.watchanime.dev";
 export const NOTIFICATION_BASE_URL_V2 = "https://notifications.watchanime.dev/v2/graphql";
 
-export const GOGO_DOWNLOAD_LINK = "https://goone.pro";
+export const GOGO_DOWNLOAD_LINK = "https://s3taku.com";
 export const STATIC_BUCKET_URL = "https://webstatic.watchanime.dev/watchanime-static";
 export const AUTH_BASE_URL = "https://auth.watchanime.dev";
 

45b9fc9d40d6a1731a845a4242f33ac959f594ff
Shounak Ghosh <sghosh1810@gmail.com>
2024-10-21 21:35:39 +0530

    whitelist anzeat

--- frontend/src/custom/AnimeData.js
+++ frontend/src/custom/AnimeData.js
@@ -47,7 +47,7 @@ const prepareVideoData = (videoData, userDefinedProxyUrl) => {
 };
 
 const getProxyUrl = (videoUrl, userDefinedProxyUrl = null) => {
-    var whitelist = ["cache", "wix", "sharepoint", "pstatic.net", "workfields", "akamai-video-content", "wetransfer", "bilucdn", "cdnstream", "vipanicdn", "anifastcdn", "mirrorakam.akamaized", document.location.hostname, "watchanime.dev"];
+    var whitelist = ["cache", "wix", "sharepoint", "pstatic.net", "workfields", "akamai-video-content", "wetransfer", "bilucdn", "cdnstream", "vipanicdn", "anifastcdn", "mirrorakam.akamaized", document.location.hostname, "watchanime.dev", "anzeat"];
     // const whitelist = [];
     if (whitelist.some((link) => videoUrl.includes(link) || videoUrl.match(/[/]{2}[w]{3}[x][^.]*/gi))) {
         return videoUrl;
